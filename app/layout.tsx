d09861c6de61b8b7d7a0a484cd68bb1451b9067e
Bilal <bilalelkaderi@hotmail.com>
2026-04-07 14:11:37 +0200

    Voeg huisje favicon toe

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'WoningAlert NL – Nooit meer een woning missen',
   description: 'Automatische woningmeldingen voor huurwoningen in Noord-Brabant.',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🏠</text></svg>',
+  },
 }
 
 export default function RootLayout({
